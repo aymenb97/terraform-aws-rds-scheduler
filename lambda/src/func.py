aba8fa3358fd20c0af4a6b992f0ebfe9f4fa111e
mcetn <ayman.bellakhder@pi-square.io>
2022-05-26 17:02:54 +0100

    lambda code

--- lambda/src/func.py
+++ lambda/src/func.py
@@ -1,76 +1,28 @@
-import boto3
 import json
-import os
-
-START_INSTANCES_EVENT = os.environ['START_INSTANCES_EVENT'] if "START_INSTANCES_EVENT" in os.environ else "START_INSTANCES_EVENT"
-STOP_INSTANCES_EVENT = os.environ['STOP_INSTANCES_EVENT'] if "STOP_INSTANCES_EVENT" in os.environ is not None else "STOP_INSTANCES_EVENT"
-REGION = os.environ["REGION"]
-TAG_KEY = os.environ["TAG_KEY"]
-TAG_VALUE = os.environ["TAG_VALUE"]
-TOPIC_ARN = os.environ["TOPIC_ARN"]
+import boto3
 
 
-def handler(event, context):
+def lambda_handler(event, context):
     print(event)
-    start_stop_instances(event["eventType"], TAG_KEY, TAG_VALUE)
+    rds = boto3.client("rds")
+    resourceGroup = boto3.client('resourcegroupstaggingapi')
+    resources = resourceGroup.get_resources(TagFilters=[{'Key': 'rds-scheduled-start-stop', 'Values':
+                                                         ['true']}])
 
+    for res in resources['ResourceTagMappingList']:
+        print(rds.describe_db_instances(
+            DBInstanceIdentifier=res['ResourceARN']))
 
-def start_stop_instances(eventType, tagKey, tagValue):
-    rds = boto3.client('rds', region_name=REGION)
-    #sns = boto3.client('sns', region_name=REGION)
-    instancelist = []
-    if(eventType == START_INSTANCES_EVENT):
-        instances = rds.describe_instances(
-            Filters=[
-                {
-                    'Name': 'tag:'+tagKey,
-                    'Values': [tagValue]
-                },
-                {
-                    'Name': 'instance-state-code',
-                    'Values': ["80"],
-                }
-            ]
-        )
-        for reservation in (instances["Reservations"]):
-            for instance in reservation["Instances"]:
-                instancelist.append(instance["InstanceId"])
-        if(len(instancelist) > 0):
-            print("Started Instances with IDs", instancelist)
-            # sns.publish(
-            #     TopicArn='arn:aws:sns:us-east-1:974195321489:scheduled-start-stop-topic',
-            #     Message="Your Scheduled Event Started {instanceCount} EC2 Instances".format(
-            #         instanceCount=len(instancelist)),
-            #     Subject="[Scheduled Start Stop] Started instances"
-            # )
+    #     print(rds.stop_db_instance(
+    #         DBInstanceIdentifier=resource['ResourceARN'].split(":")[6]))
 
-            rds.start_instances(InstanceIds=instancelist)
-        else:
-            print("No instances to stop")
-    if(eventType == STOP_INSTANCES_EVENT):
-        instances = rds.describe_instances(
-            Filters=[
-                {
-                    'Name': 'tag:'+tagKey,
-                    'Values': [tagValue]
-                },
-                {
-                    'Name': 'instance-state-code',
-                    'Values': ["16"],
-                }
-            ]
-        )
-        for reservation in (instances["Reservations"]):
-            for instance in reservation["Instances"]:
-                instancelist.append(instance["InstanceId"])
-        if(len(instancelist) > 0):
-            print("Stopped Instances with IDs", instancelist)
-            rds.stop_instances(InstanceIds=instancelist)
-            # sns.publish(
-            #     TopicArn='arn:aws:sns:us-east-1:974195321489:scheduled-start-stop-topic',
-            #     Message="Your Scheduled Event Stopped {instanceCount} EC2 Instances".format(
-            #         instanceCount=len(instancelist)),
-            #     Subject="[Scheduled Start Stop] Stopped instances"
-            # )
-        else:
-            print("No instances to start")
+    # db_instances = rds.describe_db_instances()["DBInstances"]
+    # print(rds.describe_db_instances(
+    #     DBInstanceIdentifier="arn:aws:rds:us-east-1:974195321489:db:database-2"))
+    # for instance in db_instances:
+    #     print(instance['DBInstanceIdentifier'])
+    #     if instance["TagList"][0] == "scheduled" and instance["TagList"][1] == "true":
+    #         print(instance)
+
+    # print(rds.describe_db_instances()["DBInstances"])
+    # print(rds.stop_db_instance(DBInstanceIdentifier="database-1"))
